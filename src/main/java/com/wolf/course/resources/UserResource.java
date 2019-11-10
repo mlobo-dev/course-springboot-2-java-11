e40e29ca78a4302428ce8b3d458012de711cbc1e
mlobo-dev <mlobo.dev@gmail.com>
2019-11-10 16:05:31 -0300

    Service Layer component registration

--- src/main/java/com/wolf/course/resources/UserResource.java
+++ src/main/java/com/wolf/course/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.wolf.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wolf.course.entities.User;
+import com.wolf.course.services.UserService;
 
 @RestController
-@RequestMapping(value="/users")
+@RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User>	findAll(){
-		User u = new User(1L,"Mary","mary@mail","99999999","12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
