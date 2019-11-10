7506bf94625b2c58db1d6b5a051db2fde4dacbe8
mlobo-dev <mlobo.dev@gmail.com>
2019-11-10 16:37:14 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/wolf/course/entities/User.java
+++ src/main/java/com/wolf/course/entities/User.java
@@ -1,31 +1,42 @@
 package com.wolf.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	private String name;
-	
+
 	private String email;
-	
+
 	private String phone;
-	
+
 	private String password;
-	
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
+
 	public User() {
-		
+
 	}
 
 	public User(Long id, String name, String email, String phone, String password) {
@@ -77,6 +88,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -101,7 +116,5 @@ public class User implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
+
 }
